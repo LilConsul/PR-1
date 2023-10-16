3dc17153dab90d9855360a13bfd50516f7579394
Denis Shevchenko <denshevchenko19@gmail.com>
2023-10-16 08:48:10 +0200

    added class Table.cs

--- Form1.cs
+++ Form1.cs
@@ -13,10 +13,5 @@ namespace PR_1 {
         public Form1() {
             InitializeComponent();
         }
-
-
-        private void label1_Click(object sender, EventArgs e) {
-            throw new System.NotImplementedException();
-        }
     }
 }
